dac48c449a8e52b7dc74d2e60da304bb22367c2c
Abhilash D K <abhilashdk2014@outlook.com>
2021-10-04 20:50:55 +0530

    Context

---
+++ concurrency/07-context/02-context/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+type data struct {
+	result string
+}
+
+func main() {
+
+	deadline := time.Now().Add(100 * time.Millisecond)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
+
+	compute := func() <-chan data {
+		ch := make(chan data)
+		go func() {
+			defer close(ch)
+			deadline, ok := ctx.Deadline()
+
+			if ok {
+				if deadline.Sub(time.Now().Add(50*time.Millisecond)) < 0 {
+					fmt.Println("No sufficient time given..terminating")
+					return
+				}
+			}
+			// Simulate work.
+			time.Sleep(50 * time.Millisecond)
+
+			// Report result.
+			select {
+			case ch <- data{"123"}:
+			case <-ctx.Done():
+				return
+			}
+
+		}()
+		return ch
+	}
+
+	// Wait for the work to finish. If it takes too long move on.
+	ch := compute()
+	d, ok := <-ch
+	if ok {
+		fmt.Printf("work complete: %s\n", d)
+	}
+
+}
